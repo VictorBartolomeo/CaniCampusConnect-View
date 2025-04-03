dda4ec47e7a4e971f1983bfa153f114c0b69911b
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-03 14:13:19 +0200

    updating routes with home,login,register,CGU, notfound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {LandingPageComponent} from './pages/landing-page/landing-page.component';
+import {NotFoundComponent} from './pages/not-found/not-found.component';
+import {LoginPageComponent} from './pages/login-page/login-page.component';
+import {RegisterPageComponent} from './pages/register-page/register-page.component';
+import {GeneralConditionsUseComponent} from './pages/general-conditions-use/general-conditions-use.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path : "", redirectTo :"home", pathMatch :"full"},
+  {path : "home", component : LandingPageComponent},
+  {path : "login", component : LoginPageComponent},
+  {path : "register", component : RegisterPageComponent},
+  {path : "CGU", component : GeneralConditionsUseComponent},
+  {path : "**", component : NotFoundComponent}
+];
