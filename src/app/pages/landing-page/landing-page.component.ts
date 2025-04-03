0a50cb52e1144f43debf92a0343a16513d4dce46
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-03 14:11:47 +0200

    create landing page page

---
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {LandingNavbarComponent} from '../../components/landing-navbar/landing-navbar.component';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [LandingNavbarComponent],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
+})
+export class LandingPageComponent {
+
+}
