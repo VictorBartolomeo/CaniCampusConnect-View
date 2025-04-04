336d8183f9e3c2c9215111452bb8f39cc4c35d25
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-04 15:57:12 +0200

    remove NgOptimized image

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,14 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {LoginFormComponent} from '../../components/login-form/login-form.component';
 import {LandingNavbarComponent} from '../../components/landing-navbar/landing-navbar.component';
-import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
   imports: [
     LoginFormComponent,
-    LandingNavbarComponent,
-    NgOptimizedImage
+    LandingNavbarComponent
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
