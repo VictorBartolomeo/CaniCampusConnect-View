e250c413984d9e1f40cc892ef7d99685914338ed
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-04 15:57:40 +0200

    Remove login form from register page

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,14 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {LandingNavbarComponent} from '../../components/landing-navbar/landing-navbar.component';
 import {RegisterFormComponent} from '../../components/register-form/register-form.component';
-import {LoginFormComponent} from '../../components/login-form/login-form.component';
 
 @Component({
   selector: 'app-register-page',
   imports: [
     LandingNavbarComponent,
-    RegisterFormComponent,
-    LoginFormComponent
+    RegisterFormComponent
   ],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss'
