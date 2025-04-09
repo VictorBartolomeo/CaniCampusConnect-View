b40b7c58ac68c289a152d9c820a75ac73e34a545
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-09 11:01:48 +0200

    remove a deprecated dependency

--- src/app/components/landing-navbar/landing-navbar.component.ts
+++ src/app/components/landing-navbar/landing-navbar.component.ts
@@ -1,14 +1,12 @@
 import {Component} from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
-import {NgOptimizedImage} from '@angular/common';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-landing-navbar',
-  imports: [MatMenuModule, NgOptimizedImage,
-    MatButtonModule, RouterLink, RouterLinkActive, MatIconModule
+  imports: [MatMenuModule, MatButtonModule, RouterLink, RouterLinkActive, MatIconModule
   ],
   templateUrl: './landing-navbar.component.html',
   styleUrl: './landing-navbar.component.scss'
