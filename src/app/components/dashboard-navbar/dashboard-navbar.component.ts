975036ecf946271be8d7a532f0892c1a08326770
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-09 09:21:41 +0200

    create navbar for dashboard view

--- src/app/components/dashboard-navbar/dashboard-navbar.component.ts
+++ src/app/components/dashboard-navbar/dashboard-navbar.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatFormField, MatLabel} from '@angular/material/input';
+import {MatSelect} from '@angular/material/select';
+import {MatOption} from '@angular/material/core';
 
 @Component({
   selector: 'app-dashboard-navbar',
-  imports: [],
+  imports: [
+    MatLabel,
+    MatSelect,
+    MatSelect,
+    MatOption,
+    MatFormField
+  ],
   templateUrl: './dashboard-navbar.component.html',
   styleUrl: './dashboard-navbar.component.scss'
 })
 export class DashboardNavbarComponent {
-
+  selected = 'option2';
 }
