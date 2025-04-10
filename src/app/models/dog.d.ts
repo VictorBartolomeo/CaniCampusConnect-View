0ffa63679772bc3514fe68067250dc58104098f1
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-10 18:41:50 +0200

    nullable some variables

--- src/app/models/dog.d.ts
+++ src/app/models/dog.d.ts
@@ -3,17 +3,17 @@ import {Registration} from "./registration.d.ts";
 type Dog = {
   id: number;
   name: string;
-  birthDate: string | null;
+  birthDate?: string | null;
   isMale?: boolean;
-  isSociable: boolean;
-  isInHeat: boolean;
-  isCrossbreed: boolean;
-  chipNumber: string | null;
+  isSociable?: boolean;
+  isInHeat?: boolean;
+  isCrossbreed?: boolean;
+  chipNumber?: string | null;
   breed?: Breed;
   registrations?: Registration[] | null;
   vaccinations?: Vaccination[] | null;
   veterinaryVisits?: VeterinaryVisit[] | null;
   medicationTreatments?: MedicationTreatment[] | null;
   dogWeights?: DogWeight[] | null;
-  avatarUrl?: string;
+  avatarUrl?: string | null;
 }
