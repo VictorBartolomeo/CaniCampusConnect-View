d92ad7c92aeea435912a2999eb27ce5a87d3ba1f
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-30 16:12:50 +0200

    Moved `DashboardNavbar` into routing with nested routes for better structure.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,19 @@ import {LoginPageComponent} from './pages/login-page/login-page.component';
 import {RegisterPageComponent} from './pages/register-page/register-page.component';
 import {GeneralConditionsUseComponent} from './pages/general-conditions-use/general-conditions-use.component';
 import {DashboardUserPageComponent} from './pages/dashboard-user-page/dashboard-user-page.component';
+import {DashboardNavbarComponent} from './components/dashboard-navbar/dashboard-navbar.component';
 
 export const routes: Routes = [
   {path : "", redirectTo :"home", pathMatch :"full"},
+  {path : "", component: DashboardNavbarComponent,
+    children:[
+      {path : "dashboard", component : DashboardUserPageComponent},
+      {path: "", redirectTo: "dashboard", pathMatch: "full"},
+      {path : "**", component : NotFoundComponent}
+    ]},
   {path : "home", component : LandingPageComponent},
   {path : "login", component : LoginPageComponent},
   {path : "register", component : RegisterPageComponent},
   {path : "CGU", component : GeneralConditionsUseComponent},
-  {path : "dashboard", component : DashboardUserPageComponent},
   {path : "**", component : NotFoundComponent}
 ];
--- src/app/pages/dashboard-user-page/dashboard-user-page.component.ts
+++ src/app/pages/dashboard-user-page/dashboard-user-page.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {DashboardNavbarComponent} from '../../components/dashboard-navbar/dashboard-navbar.component';
+import {CourseCardComponent} from '../../components/course-card/course-card.component';
 
 @Component({
   selector: 'app-dashboard-user-page',
+  standalone:true,
   imports: [
-    DashboardNavbarComponent
+    DashboardNavbarComponent,
+    CourseCardComponent
   ],
   templateUrl: './dashboard-user-page.component.html',
   styleUrl: './dashboard-user-page.component.scss'
