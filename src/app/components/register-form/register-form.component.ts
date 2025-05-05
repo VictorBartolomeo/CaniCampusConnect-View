ed55898a2f1f6b77227568dfeba1bf0141a7b639
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-05 10:55:37 +0200

    Add Primeng Button to landing page and remove unused imports from register form component

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,13 +1,8 @@
 import {Component, signal} from '@angular/core';
-import {RouterLink} from "@angular/router";
-import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-register-form',
-  imports: [
-    RouterLink,
-    NgOptimizedImage
-  ],
+  imports: [],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.scss'
 })
