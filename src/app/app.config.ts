f80817ffeba9fb5ae001ff2f5662df2d92965e6d
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-06 16:25:45 +0200

    Add French locale support and update PrimeNG theme configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, LOCALE_ID, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {providePrimeNG} from 'primeng/config';
@@ -10,13 +10,14 @@ import {provideHttpClient} from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
+    { provide: LOCALE_ID, useValue: 'fr-FR' },
     provideRouter(routes),
     provideHttpClient(),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Aura,
-        options : {darkModeSelector : '.p-dark'}
+        options :{prefix: 'my'}
       }
     })
   ]
