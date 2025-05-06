120293eb7ce7227ada7655d64c1d91db11f0a896
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-06 16:26:57 +0200

    "Integrate carousel to display course cards with responsive design and enhanced styling."

--- src/app/components/course-card/course-card.component.ts
+++ src/app/components/course-card/course-card.component.ts
@@ -1,28 +1,59 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Card} from 'primeng/card';
+import {Chip} from 'primeng/chip';
+import {DatePipe} from '@angular/common';
+import {Carousel} from 'primeng/carousel';
+import {PrimeTemplate} from 'primeng/api';
 
 @Component({
   selector: 'app-course-card',
-  imports: [],
+  imports: [
+    Card,
+    Chip,
+    DatePipe,
+    Carousel,
+    PrimeTemplate
+  ],
   templateUrl: './course-card.component.html',
-  styleUrl: './course-card.component.scss'
+  styleUrl: './course-card.component.scss',
 })
 export class CourseCardComponent implements OnInit {
   http = inject(HttpClient);
   courses: Course[] = [];
-  courseTypes: CourseType[] = [];
-  isLoading: boolean = true;
+  responsiveOptions: any[] | undefined;
 
   ngOnInit() {
     this.http.get<Course[]>('http://localhost:8080/courses').subscribe({
       next: (courses) => {
         this.courses = courses;
-        this.isLoading = false;
+        console.log(courses);
       },
       error: (error) => {
         console.error('Error fetching courses:', error);
-        this.isLoading = false;
       },
     });
+    this.responsiveOptions = [
+      {
+        breakpoint: '1400px',
+        numVisible: 2,
+        numScroll: 1
+      },
+      {
+        breakpoint: '1199px',
+        numVisible: 3,
+        numScroll: 1
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 2,
+        numScroll: 1
+      },
+      {
+        breakpoint: '575px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ]
   }
 }
