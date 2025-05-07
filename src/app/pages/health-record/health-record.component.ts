a799e92cc8af16c04129e3be8d92a678f6a0a4e6
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-07 09:06:47 +0200

    Add HealthRecordComponent with initial structure and test setup

---
+++ src/app/pages/health-record/health-record.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-health-record',
+  imports: [],
+  templateUrl: './health-record.component.html',
+  styleUrl: './health-record.component.scss'
+})
+export class HealthRecordComponent {
+
+}
