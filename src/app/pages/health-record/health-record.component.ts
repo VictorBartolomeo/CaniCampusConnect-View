8632a5bc9b1aede37d342865d8f285c30bdac146
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-07 10:36:55 +0200

    "Replace placeholder text with weight chart component and update HealthRecordComponent with necessary imports."

--- src/app/pages/health-record/health-record.component.ts
+++ src/app/pages/health-record/health-record.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Dog} from '../../models/dog';
+import {CommonModule} from '@angular/common';
+import {ChartModule} from 'primeng/chart';
+import {FormsModule} from '@angular/forms';
+import {WeightChartComponent} from '../../components/weight-chart/weight-chart.component';
+
 
 @Component({
   selector: 'app-health-record',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ChartModule, FormsModule, WeightChartComponent
+  ],
   templateUrl: './health-record.component.html',
   styleUrl: './health-record.component.scss'
 })
 export class HealthRecordComponent {
+
 
 }
