a8d8b046277215191689dd783a0427a0524fc020
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-07 14:25:38 +0200

    Add VaccinationTableComponent to health-record component imports

--- src/app/pages/health-record/health-record.component.ts
+++ src/app/pages/health-record/health-record.component.ts
@@ -5,12 +5,13 @@ import {CommonModule} from '@angular/common';
 import {ChartModule} from 'primeng/chart';
 import {FormsModule} from '@angular/forms';
 import {WeightChartComponent} from '../../components/weight-chart/weight-chart.component';
+import {VaccinationTableComponent} from '../../components/vaccination-table/vaccination-table.component';
 
 
 @Component({
   selector: 'app-health-record',
   standalone: true,
-  imports: [CommonModule, ChartModule, FormsModule, WeightChartComponent
+  imports: [CommonModule, ChartModule, FormsModule, WeightChartComponent, VaccinationTableComponent
   ],
   templateUrl: './health-record.component.html',
   styleUrl: './health-record.component.scss'
