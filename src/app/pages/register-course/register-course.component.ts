b5cbe391a3a2488e4249c5eb5b3cc601be459fe7
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-09 09:58:18 +0200

    Add RegisterCourseComponent with initial structure and basic test setup

---
+++ src/app/pages/register-course/register-course.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-course',
+  imports: [],
+  templateUrl: './register-course.component.html',
+  styleUrl: './register-course.component.scss'
+})
+export class RegisterCourseComponent {
+
+}
