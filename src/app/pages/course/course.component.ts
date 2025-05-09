516c882da031bfc6a664534c253b358277ebe9ae
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-09 09:19:20 +0200

    Add CalendarComponent to Course page template and imports.

--- src/app/pages/course/course.component.ts
+++ src/app/pages/course/course.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import {CourseCardComponent} from '../../components/course-card/course-card.component';
 import {PastCoursesComponent} from '../../components/past-courses/past-courses.component';
+import {CalendarComponent} from '../../components/calendar/calendar.component';
 
 @Component({
   selector: 'app-course',
   imports: [
     CourseCardComponent,
-    PastCoursesComponent
+    PastCoursesComponent,
+    CalendarComponent
   ],
   templateUrl: './course.component.html',
   styleUrl: './course.component.scss'
