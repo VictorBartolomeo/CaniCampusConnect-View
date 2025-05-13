6861a7ce0b53706008d51ea27c0d973a4500b3b3
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-13 21:49:19 +0200

    Add DogFormComponent to ManageDogsPage for dog form functionality

--- src/app/pages/manage-dogs-page/manage-dogs-page.component.ts
+++ src/app/pages/manage-dogs-page/manage-dogs-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {DogSummaryComponent} from '../../components/dog-summary/dog-summary.component';
+import {DogFormComponent} from '../../components/dog-form/dog-form.component';
 
 @Component({
   selector: 'app-manage-dogs-page',
   imports: [
-    DogSummaryComponent
+    DogSummaryComponent,
+    DogFormComponent
   ],
   templateUrl: './manage-dogs-page.component.html',
   styleUrl: './manage-dogs-page.component.scss'
