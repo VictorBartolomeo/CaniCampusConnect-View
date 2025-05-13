14a74dc232a786084396c19f4c2b1da6e9032297
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-13 20:46:17 +0200

    Add Owner type definition to models

---
+++ src/app/models/owner.d.ts
@@ -0,0 +1,14 @@
+import {Dog} from "./dog";
+
+export type Owner = {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone?: string;
+  registrationDate?: string;
+  address?: string;
+  dogs?: Dog[];
+  active?: boolean;
+
+}
