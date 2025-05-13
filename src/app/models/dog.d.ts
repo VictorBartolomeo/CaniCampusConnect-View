330c60795a8ac2481e8af32339e443ebefb9f3ff
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-13 20:46:54 +0200

    "Remove unused Vaccine import from dog model."

--- src/app/models/dog.d.ts
+++ src/app/models/dog.d.ts
@@ -1,6 +1,6 @@
 import {Registration} from "./registration.d.ts";
 import {DogWeight} from './dogweight';
-import {Vaccination, Vaccine} from './vaccination';
+import {Vaccination} from './vaccination';
 import {VeterinaryVisit} from './veterinaryvisit';
 
 type Dog = {
