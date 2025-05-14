9ffc4ec45677e56351ab4258de227af93d45d81f
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-14 16:12:57 +0200

    Add DogAddFormComponent to ManageDogsPage component imports.

--- src/app/pages/manage-dogs-page/manage-dogs-page.component.ts
+++ src/app/pages/manage-dogs-page/manage-dogs-page.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {DogSummaryComponent} from '../../components/dog-summary/dog-summary.component';
 import {DogFormComponent} from '../../components/dog-form/dog-form.component';
 import {Splitter} from 'primeng/splitter';
+import {DogAddFormComponent} from '../../components/dog-add-form/dog-add-form.component';
 
 @Component({
   selector: 'app-manage-dogs-page',
   imports: [
     DogSummaryComponent,
     DogFormComponent,
-    Splitter
+    Splitter,
+    DogAddFormComponent
   ],
   templateUrl: './manage-dogs-page.component.html',
   styleUrl: './manage-dogs-page.component.scss'
