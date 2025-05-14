56d8210758920d38f85ad6f526305818278ab42c
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-14 14:55:17 +0200

    Add Splitter component to ManageDogsPage for layout management.

--- src/app/pages/manage-dogs-page/manage-dogs-page.component.ts
+++ src/app/pages/manage-dogs-page/manage-dogs-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {DogSummaryComponent} from '../../components/dog-summary/dog-summary.component';
 import {DogFormComponent} from '../../components/dog-form/dog-form.component';
+import {Splitter} from 'primeng/splitter';
 
 @Component({
   selector: 'app-manage-dogs-page',
   imports: [
     DogSummaryComponent,
-    DogFormComponent
+    DogFormComponent,
+    Splitter
   ],
   templateUrl: './manage-dogs-page.component.html',
   styleUrl: './manage-dogs-page.component.scss'
