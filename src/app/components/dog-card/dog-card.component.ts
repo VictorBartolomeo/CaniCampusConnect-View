b9bc19bc724efef7a45ae58cbca08591f09e5c20
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-18 15:02:41 +0200

    Add initial DogCardComponent with template, styles, and unit test setup

---
+++ src/app/components/dog-card/dog-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dog-card',
+  imports: [],
+  templateUrl: './dog-card.component.html',
+  styleUrl: './dog-card.component.scss'
+})
+export class DogCardComponent {
+
+}
