96dda0c999f4534720c100ad50ed77242153ba40
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-18 15:03:03 +0200

    Add DogCardComponent to OwnerProfile page.

--- src/app/pages/owner-profile/owner-profile.component.ts
+++ src/app/pages/owner-profile/owner-profile.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {DogCardComponent} from '../../components/dog-card/dog-card.component';
 
 @Component({
   selector: 'app-owner-profile',
-  imports: [],
+  imports: [
+    DogCardComponent
+  ],
   templateUrl: './owner-profile.component.html',
   styleUrl: './owner-profile.component.scss'
 })
