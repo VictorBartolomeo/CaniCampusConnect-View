d58e968cac41c693991ba83fd6418735a0047043
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-22 16:21:52 +0200

    "Remove redundant comments from user service methods."

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -17,19 +17,16 @@ export class UserService {
   ) {
   }
 
-  // Récupérer les informations de l'utilisateur (Owner) connecté
   getCurrentUser(): Observable<Owner> {
     const userId = this.authService.getUserId();
     return this.http.get<Owner>(`${this.apiUrl}/owner/${userId}`);
   }
 
-  // Mettre à jour les informations de l'utilisateur (Owner)
   updateUser(owner: Partial<Owner>): Observable<Owner> {
     const userId = this.authService.getUserId();
     return this.http.put<Owner>(`${this.apiUrl}/owner/${userId}`, owner);
   }
 
-  // Mettre à jour le mot de passe (à implémenter selon votre API)
   updatePassword(currentPassword: string, newPassword: string): Observable<any> {
     const userId = this.authService.getUserId();
     return this.http.put(`${this.apiUrl}/owner/${userId}/password`, {
