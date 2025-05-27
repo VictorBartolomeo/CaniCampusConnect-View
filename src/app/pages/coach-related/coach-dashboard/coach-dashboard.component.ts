a244799a6c8d3f840e7a5086cd12168215f24ca7
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-27 15:53:35 +0200

    "Enhance CoachDashboard with tabs, date picker, and ICS download functionality."

--- src/app/pages/coach-related/coach-dashboard/coach-dashboard.component.ts
+++ src/app/pages/coach-related/coach-dashboard/coach-dashboard.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {Tab, TabList, Tabs} from 'primeng/tabs';
+import {FormsModule} from '@angular/forms';
+import {DatePicker} from 'primeng/datepicker';
+import {Button} from 'primeng/button';
 
 @Component({
   selector: 'app-coach-dashboard',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    Tabs,
+    Tab,
+    RouterLink,
+    TabList,
+    FormsModule,
+    DatePicker,
+    Button
+  ],
   templateUrl: './coach-dashboard.component.html',
   styleUrl: './coach-dashboard.component.scss'
 })
 export class CoachDashboardComponent {
+  tabs = [
+    { route: '/coach/dashboard/user/course', label: 'Cours', icon: 'pi pi-calendar-clock' },
+    { route: '/coach/dashboard/user/chat', label: 'Messagerie', icon: 'pi pi-comments', disabled: true },
+    { route: '/coach/dashboard/user/health-record', label: 'Carnet de Santé', icon: 'pi pi-chart-bar' }
+  ];
+  dates: Date[] | undefined;
 
+  loading: boolean = false;
+
+  load() {
+    this.loading = true;
+
+    setTimeout(() => {
+      this.loading = false
+    }, 2000);
+  }
 }
