bb315834849afad89c332cb67a2c0b01db9bb657
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-27 16:49:26 +0200

    Add initial implementation of CoachCalendarComponent with HTML, SCSS, and unit tests.

---
+++ src/app/components/coach-related/coach-calendar/coach-calendar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coach-calendar',
+  imports: [],
+  templateUrl: './coach-calendar.component.html',
+  styleUrl: './coach-calendar.component.scss'
+})
+export class CoachCalendarComponent {
+
+}
