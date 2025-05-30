f2f8cf21b4a1ea65be9ef583de2d7867c26c23b4
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-30 13:40:09 +0200

    "Remove CourseCardComponent import from LandingPageComponent to clean up unused dependencies."

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,11 +1,10 @@
 import {Component} from '@angular/core';
 import {LandingNavbarComponent} from '../../components/landing-navbar/landing-navbar.component';
-import {CourseCardComponent} from '../../components/owner-related/course-card/course-card.component';
 import {Button} from 'primeng/button';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [LandingNavbarComponent, CourseCardComponent, Button],
+  imports: [LandingNavbarComponent, Button],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
