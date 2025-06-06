721eb013298b8290cb4402330b119ea5f438156a
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-06-06 13:54:47 +0200

    Add ForgotPasswordPageComponent: implement layout with navbar and forgot password form.

---
+++ src/app/pages/forgot-password-page/forgot-password-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {LandingNavbarComponent} from '../../components/landing-navbar/landing-navbar.component';
+import {ForgotPasswordFormComponent} from '../../components/forgot-password-form/forgot-password-form.component';
+
+@Component({
+  selector: 'app-forgot-password-page',
+  imports: [
+    LandingNavbarComponent,
+    ForgotPasswordFormComponent
+  ],
+  templateUrl: './forgot-password-page.component.html',
+  styleUrl: './forgot-password-page.component.scss'
+})
+export class ForgotPasswordPageComponent {
+
+}
