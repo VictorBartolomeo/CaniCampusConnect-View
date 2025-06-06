5fe653a15afb113204bf35813fbaf207c6773f45
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-06-06 16:00:18 +0200

    Refactor DogService: remove unnecessary comment in constructor.

--- src/app/service/dog.service.ts
+++ src/app/service/dog.service.ts
@@ -25,7 +25,6 @@ export class DogService {
     private userService: UserService,
     private breedService: BreedService
   ) {
-    // S'abonner aux changements d'ID utilisateur pour charger les chiens
     this.authStateService.userId$.subscribe(userId => {
       if (userId) {
         this.loadUserDogs(userId);
