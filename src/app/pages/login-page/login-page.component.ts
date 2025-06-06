71dba40e3c16323b23c8f32912c08dc23b0d603a
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-06-06 15:58:46 +0200

    Refactor LoginPageComponent: remove LandingNavbarComponent, adjust layout styles, and update imports.

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,10 @@
 import {Component} from '@angular/core';
 import {LoginFormComponent} from '../../components/login-form/login-form.component';
-import {LandingNavbarComponent} from '../../components/landing-navbar/landing-navbar.component';
 
 @Component({
   selector: 'app-login-page',
   imports: [
-    LoginFormComponent,
-    LandingNavbarComponent
+    LoginFormComponent
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
