dac1153f5af503d839d12b4e0ee56d6f9035fe94
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-06-06 13:56:02 +0200

    Add GeneralConditionsUseComponent: implement layout, include necessary modules, and define content structure.

--- src/app/pages/general-conditions-use/general-conditions-use.component.ts
+++ src/app/pages/general-conditions-use/general-conditions-use.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { ScrollTopModule } from 'primeng/scrolltop';
 
 @Component({
   selector: 'app-general-conditions-use',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterLink,
+    CardModule,
+    ButtonModule,
+    DividerModule,
+    ScrollTopModule
+  ],
   templateUrl: './general-conditions-use.component.html',
   styleUrl: './general-conditions-use.component.scss'
 })
