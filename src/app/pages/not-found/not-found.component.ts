66d6ed5489a69220d067cd4446ff4acfb60f2485
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-06-06 15:58:21 +0200

    Update NotFoundComponent: implement layout redesign, add Primeng modules, and include 404 icon asset.

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { ScrollTopModule } from 'primeng/scrolltop';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterLink,
+    CardModule,
+    ButtonModule,
+    DividerModule,
+    ScrollTopModule
+  ],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
