f32e75a65b98c51c95b79e33c5592f69e679fd73
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-06-13 19:30:24 +0200

    Add CoachListComponent with template, styles, and logic for displaying and managing a list of coaches.

---
+++ src/app/components/admin-related/coach-list/coach-list.component.ts
@@ -0,0 +1,49 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
+
+@Component({
+  selector: 'app-coach-list',
+  standalone: true,
+  imports: [
+    CommonModule,
+    TableModule,
+    ButtonModule,
+    TooltipModule
+  ],
+  templateUrl: './coach-list.component.html',
+  styleUrl: './coach-list.component.scss'
+})
+export class CoachListComponent {
+  @Input() coaches: any[] = [];
+  @Input() loading: boolean = false;
+
+  @Output() coachSelected = new EventEmitter<any>();
+  @Output() coachEdit = new EventEmitter<any>();
+  @Output() addCoachRequest = new EventEmitter<void>();
+
+  /**
+   * Emit an event to view coach details
+   * @param coach The coach to view
+   */
+  viewCoachDetails(coach: any): void {
+    this.coachSelected.emit(coach);
+  }
+
+  /**
+   * Emit an event to edit a coach
+   * @param coach The coach to edit
+   */
+  editCoach(coach: any): void {
+    this.coachEdit.emit(coach);
+  }
+
+  /**
+   * Emit an event to request adding a new coach
+   */
+  requestAddCoach(): void {
+    this.addCoachRequest.emit();
+  }
+}
