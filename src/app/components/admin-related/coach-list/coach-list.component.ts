43851812f21c1b142ab4319110c83950518fce52
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-06-15 22:52:22 +0200

    Update CoachListComponent to integrate delete button and confirmation dialog for coach deletion.

--- src/app/components/admin-related/coach-list/coach-list.component.ts
+++ src/app/components/admin-related/coach-list/coach-list.component.ts
@@ -5,6 +5,8 @@ import { ButtonModule } from 'primeng/button';
 import { TooltipModule } from 'primeng/tooltip';
 import { AddCoachButtonComponent } from '../utilities/add-coach-button/add-coach-button.component';
 import { EditCoachButtonComponent } from '../utilities/edit-coach-button/edit-coach-button.component';
+import { DeleteCoachButtonComponent } from '../utilities/delete-coach-button/delete-coach-button.component';
+import { DeleteCoachDialogComponent } from '../delete-coach-dialog/delete-coach-dialog.component';
 
 @Component({
   selector: 'app-coach-list',
@@ -15,7 +17,9 @@ import { EditCoachButtonComponent } from '../utilities/edit-coach-button/edit-co
     ButtonModule,
     TooltipModule,
     AddCoachButtonComponent,
-    EditCoachButtonComponent
+    EditCoachButtonComponent,
+    DeleteCoachButtonComponent,
+    DeleteCoachDialogComponent
   ],
   templateUrl: './coach-list.component.html',
   styleUrl: './coach-list.component.scss'
@@ -27,6 +31,11 @@ export class CoachListComponent {
   @Output() coachSelected = new EventEmitter<any>();
   @Output() coachEdit = new EventEmitter<any>();
   @Output() addCoachRequest = new EventEmitter<void>();
+  @Output() coachDelete = new EventEmitter<any>();
+
+  // Properties for delete confirmation dialog
+  deleteDialogVisible: boolean = false;
+  coachToDelete: any = null;
 
   /**
    * Emit an event to view coach details
@@ -49,5 +58,22 @@ export class CoachListComponent {
    */
   requestAddCoach(): void {
     this.addCoachRequest.emit();
+  }
+
+  /**
+   * Show the delete confirmation dialog
+   * @param coach The coach to delete
+   */
+  showDeleteDialog(coach: any): void {
+    this.coachToDelete = coach;
+    this.deleteDialogVisible = true;
+  }
+
+  /**
+   * Handle the confirmation of coach deletion
+   * @param coach The coach to delete
+   */
+  onConfirmDelete(coach: any): void {
+    this.coachDelete.emit(coach);
   }
 }
