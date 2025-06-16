954392db24683c6c0288fc83fee130eae61e9e32
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-06-16 14:29:19 +0200

    Add environment files for development and production, and update .gitignore accordingly.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+};
+
+export const API_CONFIG_URL = {
+  apiUrl: 'http://localhost:8080'
+};
+
+export const userLogins = {
+  ownerLog: 'owner3@user.com',
+  ownerPass : 'CaniC4mpus57*'
+}
+
+export const coachLogins = {
+  coachLog: 'coach@coach.com',
+  coachPass : 'CaniC4mpus57*'
+}
