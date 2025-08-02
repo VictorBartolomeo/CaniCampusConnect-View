1210e0ddf5014a4538f07c8ecd5ad0e911ad9d8a
Victor Monteragioni <victor.monteriggioni@gmail.com>
2025-08-02 17:00:35 +0200

    Add tab navigation with routes and icons to admin dashboard component.

--- src/app/pages/admin-related/admin-dashboard/admin-dashboard.component.ts
+++ src/app/pages/admin-related/admin-dashboard/admin-dashboard.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {Tab, TabList, Tabs} from 'primeng/tabs';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, Tab, TabList, Tabs, RouterLink, RouterLinkActive],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss'
 })
 export class AdminDashboardComponent {
-
+  tabs = [
+    { route: '/admin/dashboard/general/business-intelligence', label: 'CaniBI', icon: 'pi pi-calendar-clock' },
+    { route: '/admin/dashboard/calendar', label: 'Calendrier', icon: 'pi pi-calendar' },
+    { route: '/admin/dashboard/chat', label: 'Messagerie', icon: 'pi pi-comments', disabled: true },
+    { route: '/admin/dashboard/health-record', label: 'Carnet de Santé', icon: 'pi pi-chart-bar' }
+  ];
 }
